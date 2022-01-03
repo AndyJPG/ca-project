f6abf152dd5b8127944a6bbf85066f18e5af39e8
Andy Jiang <peigeng.jiang@preezie.com>
2022-01-03 12:36:02 +1100

    finish basic layout for home page

--- packages/client/src/components/SearchBar/SearchBar.style.tsx
+++ packages/client/src/components/SearchBar/SearchBar.style.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import {InputBase, styled} from "@mui/material"
+import {Button, InputBase, styled} from "@mui/material"
 import SearchIcon from '@mui/icons-material/Search'
 
 export interface SearchBarStyles {
@@ -11,7 +11,7 @@ interface SearchBarStyleProps {
     styles?: SearchBarStyles
 }
 
-const SearchBar = styled('div')(() => ({
+const SearchBar = styled('div')(({theme}) => ({
     position: 'relative',
     display: 'flex',
     alignItems: 'center',
@@ -21,20 +21,43 @@ const SearchBar = styled('div')(() => ({
     width: '100%',
     borderRadius: '999px',
     backgroundColor: 'white',
+    [theme.breakpoints.up("md")]: {
+        padding: "9px",
+        justifyContent: 'space-between'
+    }
 }))
 
-const StyledInputBase = styled(InputBase)(() => ({
+const StyledInputBase = styled(InputBase)(({theme}) => ({
     fontSize: '14px',
-    minWidth: '180px'
+    minWidth: '200px',
+    [theme.breakpoints.up("md")]: {
+        marginLeft: "18px",
+        fontSize: '16px',
+        minWidth: '240px'
+    }
 }))
 
-const SearchBarIconWrapper = styled('div')(() => ({
+const SearchBarIconWrapper = styled('div')(({theme}) => ({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'relative',
     height: '100%',
-    marginRight: '8px'
+    marginRight: '8px',
+    [theme.breakpoints.up("md")]: {
+        display: 'none'
+    }
+}))
+
+const SearchBarButton = styled(Button)(({theme}) => ({
+    border: "1px solid black",
+    borderRadius: "100%",
+    padding: "11px",
+    minWidth: "auto",
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+        display: "inline-flex"
+    }
 }))
 
 const SearchBarStyle = (props: SearchBarStyleProps) => {
@@ -47,6 +70,9 @@ const SearchBarStyle = (props: SearchBarStyleProps) => {
                 <SearchIcon/>
             </SearchBarIconWrapper>
             <StyledInputBase placeholder="Where are your next home?"/>
+            <SearchBarButton>
+                <SearchIcon/>
+            </SearchBarButton>
         </SearchBar>
     )
 }
