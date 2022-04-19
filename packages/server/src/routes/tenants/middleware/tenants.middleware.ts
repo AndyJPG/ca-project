91650e162bfd755307b830d10c488389f08f4180
peigengjiang <peigeng.jiang@preezie.com>
2022-04-19 22:46:18 +1000

    added tenant middleware

--- packages/server/src/routes/tenants/middleware/tenants.middleware.ts
+++ packages/server/src/routes/tenants/middleware/tenants.middleware.ts
@@ -1,46 +1,62 @@
+import debug from "debug"
 import express from "express"
 import tenantsService from "../services/tenants.service"
 
+const log: debug.IDebugger = debug("app:tenants-controller")
+
 class TenantsMiddleware {
-    async validateTenantExists(
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-    ) {
-        const tenant = await tenantsService.readById(req.params.tenantId)
-        if (tenant) {
-            next()
-        } else {
-            res.status(404).send({
-                error: `Tenant ${req.params.tenantId} not found`
-            })
-        }
+  async validateSameCompanyDomainDoesntExist(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const tenant = await tenantsService.getTenantByCompanyDomain(req.body.companyDomain)
+    if (tenant) {
+      res.status(404).send({
+        error: "Tenant domain already exists"
+      })
+    } else {
+      next()
     }
+  }
 
-    async validateCreateTenantRequiredBodyFields(
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-    ) {
-        const {companyName} = req.body
-        if (companyName) {
-            next()
-        } else {
-            res.status(400).send({
-                error: 'Missing required fields to create tenant'
-            })
-        }
+  async validateCreateTenantRequiredBodyFields(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    if (req.body && req.body.companyName && req.body.companyDomain) {
+      next()
+    } else {
+      res.status(400).send({
+        error: "Missing required fields to create tenant"
+      })
     }
+  }
 
-    async extractTenantId(
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-    ) {
-        req.body.id = req.params.tenantId
-        next()
+  async validateTenantExists(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const tenant = await tenantsService.readById(req.params.tenantId)
+    if (tenant) {
+      next()
+    } else {
+      res.status(400).send({
+        error: `Tenant ${req.params.tenantId} not found`
+      })
     }
+  }
+
+  async extractTenantId(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    req.body.id = req.params.tenantId
+    next()
+  }
 }
 
-const tenantsMiddleware = new TenantsMiddleware()
-export default tenantsMiddleware
+export default new TenantsMiddleware()
