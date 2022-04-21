a7582237e98e0b6aba0d2a222f526a8c3041a175
peigengjiang <peigeng.jiang@preezie.com>
2022-04-21 22:44:04 +1000

    implemented global error handling strategy

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,21 +1,20 @@
-import express from "express"
-import * as http from 'http'
-
-import * as winston from "winston"
-import * as expressWinston from 'express-winston'
-import cors from 'cors'
-import {CommonRoutesConfig} from "./src/routes/common/common.routes.config"
+import cors from "cors"
 import debug from "debug"
-import {UsersRoutes} from "./src/routes/users/users.routes.config"
+import express from "express"
+import * as expressWinston from "express-winston"
+import * as http from "http"
+import * as winston from "winston"
+import { CommonRoutesConfig } from "./src/routes/common/common.routes.config"
 import ProductsRoutes from "./src/routes/products/products.routes.config"
 import TenantsRoutes from "./src/routes/tenants/tenants.routes.config"
+import { UsersRoutes } from "./src/routes/users/users.routes.config"
 
 const app: express.Application = express()
 const api: express.Application = express()
 const server: http.Server = http.createServer(app)
 const port = 3300
 const routes: Array<CommonRoutesConfig> = []
-const debugLog: debug.IDebugger = debug('app')
+const debugLog: debug.IDebugger = debug("app")
 
 // Middleware to parse all incoming requests as JSON
 app.use(express.json())
@@ -26,24 +25,24 @@ app.use(cors())
 // ExpressWinston logging middleware configuration
 // automatically log all HTTP requests handled by Express.js
 const loggerOptions: expressWinston.LoggerOptions = {
-    transports: [new winston.transports.Console()],
-    format: winston.format.combine(
-        winston.format.json(),
-        winston.format.prettyPrint(),
-        winston.format.colorize({all: true})
-    )
+  transports: [ new winston.transports.Console() ],
+  format: winston.format.combine(
+    winston.format.json(),
+    winston.format.prettyPrint(),
+    winston.format.colorize({ all: true })
+  )
 }
 
 // Check if debugging
 if (process.env.NODE_ENV !== "production") {
-    loggerOptions.meta = false
+  loggerOptions.meta = false
 }
 
 // Initialize the logger with the above configuration
 app.use(expressWinston.logger(loggerOptions))
 
 // app mounted path
-app.use('/api', api)
+app.use("/api", api)
 
 // Add routes to routes array
 routes.push(new UsersRoutes(api))
@@ -52,16 +51,23 @@ routes.push(new TenantsRoutes(api))
 
 // Simple testing routes
 const runningMessage = `Server running at http://localhost:${port}`
-app.get('/', (req: express.Request, res: express.Response) => {
+if (process.env.NODE_ENV === "development") {
+  app.get("/", (req: express.Request, res: express.Response) => {
     res.status(200).send(runningMessage)
+  })
+}
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.status(err.status || 500)
+  res.send(err)
 })
 
 // Start server
 server.listen(port, () => {
-    routes.forEach((route: CommonRoutesConfig) => {
-        debugLog(`Routes configured for ${route.getName()}`)
-    })
-    // only exception to use console.log()
-    // because we always want to know the server is done starting up
-    console.log(runningMessage)
+  routes.forEach((route: CommonRoutesConfig) => {
+    debugLog(`Routes configured for ${route.getName()}`)
+  })
+  // only exception to use console.log()
+  // because we always want to know the server is done starting up
+  console.log(runningMessage)
 })
--- packages/server/src/routes/tenants/controllers/tenants.controller.ts
+++ packages/server/src/routes/tenants/controllers/tenants.controller.ts
@@ -1,36 +1,47 @@
 import debug from "debug"
 import express from "express"
+import createHttpError from "http-errors"
 import tenantsService from "../services/tenants.service"
 
 const log: debug.IDebugger = debug("app:tenants-controller")
 
 class TenantsController {
   async listTenants(req: express.Request, res: express.Response) {
+    //TODO: Implement error handling
     const tenants = await tenantsService.list(100, 0)
     res.status(200).send(tenants)
   }
 
   async getTenantById(req: express.Request, res: express.Response) {
+    //TODO: Implement error handling
     const tenant = await tenantsService.readById(req.body.id)
     res.status(200).send(tenant)
   }
 
   async createTenant(req: express.Request, res: express.Response) {
+    //TODO: Implement error handling
     const tenantId = await tenantsService.create(req.body)
     res.status(201).send({ id: tenantId })
   }
 
   async patch(req: express.Request, res: express.Response) {
+    //TODO: Implement error handling
     log(await tenantsService.patchById(req.body.id, req.body))
     res.status(204).send()
   }
 
-  async put(req: express.Request, res: express.Response) {
-    log(await tenantsService.putById(req.body.id, req.body))
-    res.status(204).send()
+  async put(req: express.Request, res: express.Response, next: express.NextFunction) {
+    //TODO: Implement error handling
+    try {
+      log(await tenantsService.putById(req.body.id, req.body))
+      res.status(204).send()
+    } catch (e) {
+      next(createHttpError(400, "Fail to update tenant"))
+    }
   }
 
   async removeTenant(req: express.Request, res: express.Response) {
+    //TODO: Implement error handling
     log(await tenantsService.deleteById(req.body.id))
     res.status(204).send()
   }
